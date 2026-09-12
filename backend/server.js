2cf7d060ad3ac321134e24444b868cc8a73d81e0
Vivek <vivekreddy3033@gmail.com>
2026-09-12 18:38:39 +0530

    Initial TaskFlow project setup

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;;
+
+app.get("/", (req, res) => {
+    res.send("TaskFlow API is running");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
