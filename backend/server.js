afc531fb8123a576c21486185f13272f7895e37d
Vivek <vivekreddy3033@gmail.com>
2026-09-13 22:59:12 +0530

    Fix backend server for deployment

--- backend/server.js
+++ backend/server.js
@@ -40,6 +40,6 @@ mongoose
 // Start server
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+        console.log(`Server running on port ${PORT}`);
 });
