ad0ff788bca2bfb7c1241278bd3d353ee72a52ab
vivekreddy-03 <vivekreddy3033@gmail.com>
2026-09-13 23:44:39 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://taskflow-gg6t.onrender.com/api";
 
 function App() {
     const [page, setPage] = useState("login");
